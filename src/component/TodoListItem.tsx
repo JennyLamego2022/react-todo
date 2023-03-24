41c2c143eb5addeba44f405a685f38900218638b
JennyLamego2022 <jennylamego@hotmail.com>
2023-03-24 19:58:36 -0300

    feat: Criação de linha da lista

--- src/component/TodoListItem.tsx
+++ src/component/TodoListItem.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const TodoListItem = () => {
-    return (<div>Item</div>)
+    return (
+        <tr className="uk-animation-slide-bottom-medium">
+            <td className="uk-width-auto">
+                <label>
+                    <input className="uk-checkbox"
+                        type="checkbox"
+                     />
+                </label>
+            </td>
+            <td className="uk-width-expand">
+               Item
+            </td>
+            <td className="uk-width-auto">
+                <button
+                    className="uk-icon-button uk-button-danger"
+                    uk-icon="trash"
+                    ></button>
+            </td>
+        </tr>
+    );
 }
 
 export default TodoListItem;

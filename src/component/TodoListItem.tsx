06377ed441f3d813825c671a14c0ec648bdb698f
JennyLamego2022 <jennylamego@hotmail.com>
2023-03-29 17:33:33 -0300

    feat: Criação do service e funções localStorage

--- src/component/TodoListItem.tsx
+++ src/component/TodoListItem.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
+import { TodoContextType } from '../contexts/TodoContextType';
 import { Todo } from '../models/Todo';
 
 interface TodoListItemProps{
@@ -7,13 +9,15 @@ interface TodoListItemProps{
 
 const TodoListItem = (props: TodoListItemProps) => {
 
+    const { removeTodo, toggle } = useContext<TodoContextType>(TodoContext)
+
     const onRemove = (todo: Todo) => {
-        console.log(todo)
+        removeTodo(todo)
     }
     
     const handleChange = (event: any) => {
         // event.target.id
-        console.log('Mudou')
+        toggle(props.todo)
     }
     
     return (

9eb0b20eab26d723d46ba2b9d9aa82817e770abe
JennyLamego2022 <jennylamego@hotmail.com>
2023-03-29 16:24:50 -0300

    feat: Atualização de informações do Todocontext

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import TodoContext from "../contexts/TodoContext";
 import Navbar from "./NavBar";
 import TodoList from "./TodoList";
 
 const App = () => {
     return (
-    <div className="uk-container">
-        <Navbar></Navbar>
-        <TodoList></TodoList>
-    </div>)
+        <TodoContext>
+            <div className="uk-container">
+                <Navbar></Navbar>
+                <TodoList></TodoList>
+             </div>
+        </TodoContext>
+    );
 }
 
 export default App;

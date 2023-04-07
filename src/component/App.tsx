1d40376c72952dcd29b63e707f9d21ead8585bcb
JennyLamego2022 <jennylamego@hotmail.com>
2023-04-07 10:52:36 -0300

    feat: Novas bibliotecas e criação do componente AddTodo

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import TodoContext from "../contexts/TodoContext";
 import Navbar from "./NavBar";
 import TodoList from "./TodoList";
+import { BrowserRouter as Route, Router, Switch } from "react-router-dom";
+import AddTodo from './AddTodo';
+
 
 const App = () => {
     return (
         <TodoContext>
-            <div className="uk-container">
+            <Router>
                 <Navbar></Navbar>
-                <TodoList></TodoList>
-             </div>
+                <br/>
+                <div className="uk-container">
+                    <Switch>
+                        <Route path="/create">
+                            <AddTodo></AddTodo>
+                        </Route>
+                        <Route path="/">
+                            <h4>Minha lista de tarefas</h4>
+                            <TodoList></TodoList>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         </TodoContext>
     );
 }
